70e8757944873332652c6af7e16326c880ff39a1
Basharat Ali <145014089+BasharatAli00@users.noreply.github.com>
2026-02-10 00:22:39 +0500

    Enhance output formatting in csv_loader.py
    Added a blank print statement for better output formatting.

--- doc_loader/csv_loader.py
+++ doc_loader/csv_loader.py
@@ -2,6 +2,7 @@ from langchain_community.document_loaders import CSVLoader
 
 loader=CSVLoader(file_path='monthly_load.csv')
 doc=loader.load()
-print(len(doc))
+print(len(doc)) 
+print()
 print(doc.page_content)
 print(doc[1].metadata)
